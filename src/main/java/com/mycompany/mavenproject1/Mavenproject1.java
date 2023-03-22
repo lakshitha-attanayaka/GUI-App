0945df7e52a1b59e9edaa05569b5a14a605e8df5
Lakshitha Attanayaka <lakshitha.attanayaka@gmail.com>
2023-03-21 23:51:36 -0700

    init project

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Win10
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        NewJFrame f1 = new NewJFrame();
+        f1.setVisible(true);
+    }
+}
